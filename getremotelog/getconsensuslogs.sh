6b6463bce93011b92468bb79ef0cebdcbb1e3082
Igor Machado <igor.machado@gmail.com>
2019-01-12 14:15:57 -0200

    improved get remote log

--- getremotelog/getconsensuslogs.sh
+++ getremotelog/getconsensuslogs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "getting logs..."
-cp `ls ../../../neocompiler-eco/docker-compose-eco-network/logs-neocli-node1/*.log | tail -1` log3.log
-cp `ls ../../../neocompiler-eco/docker-compose-eco-network/logs-neocli-node2/*.log | tail -1` log1.log
-cp `ls ../../../neocompiler-eco/docker-compose-eco-network/logs-neocli-node3/*.log | tail -1` log4.log
-cp `ls ../../../neocompiler-eco/docker-compose-eco-network/logs-neocli-node4/*.log | tail -1` log2.log
+ECO_LOGS_PATH="../../neocompiler-eco/docker-compose-eco-network"
+cp `ls $ECO_LOGS_PATH/logs-neocli-node1/*.log | tail -1` log3.log
+cp `ls $ECO_LOGS_PATH/logs-neocli-node2/*.log | tail -1` log1.log
+cp `ls $ECO_LOGS_PATH/logs-neocli-node3/*.log | tail -1` log4.log
+cp `ls $ECO_LOGS_PATH/logs-neocli-node4/*.log | tail -1` log2.log
 
 echo "Converting logs to js format"
 ./convertlogstojs.sh
